9cd963500b0f14596c140a5c24d8cc78ecfae26f
Tatiana Kosenko <osmile378@gmail.com>
2023-09-18 17:51:50 +0300

    Added new info

---
+++ Example/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter ur name");
+string userName = Console.ReadLine();
+Console.WriteLine("Hello, ");
+Console.WriteLine(userName);
